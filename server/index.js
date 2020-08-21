6db1193e66e38e70dd0f6548457a39b29679085b
Mahoney96 <cmahoney966@gmail.com>
2020-08-21 00:30:56 -0700

    local server running

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
-require('')
+require('dotenv').config()
 
-const express = 
+const express = require('express')
+const massive = require('massive')
+
+const app = express()
+
+const SERVER_PORT= 3993
+
+app.use(express.json())
+
+
+app.listen(SERVER_PORT, () => console.log(`Hey Hey, Your Server, Works Today ${SERVER_PORT}`));
