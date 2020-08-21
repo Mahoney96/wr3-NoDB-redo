ff44b6b727a37c755449eeddbfb3f77a65ce3969
Mahoney96 <cmahoney966@gmail.com>
2020-08-21 09:45:20 -0700

    initial server and React setup complete

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const Header = () => {
+    return <div>
+        This is a header component!
+    </div>
+};
+
+export default Header;
+
+
+
+// import React, { Component } from "react";
+
+// class App extends Component {
+//     render
+// }
