ed369618015f82c0f59293ea6e64fff2c9466f88
Mahoney96 <cmahoney966@gmail.com>
2020-08-21 15:04:14 -0700

    connected to DB, focusing on routing set-up

--- server/index.js
+++ server/index.js
@@ -5,9 +5,11 @@ const massive = require('massive')
 
 const app = express()
 
+app.use(express.json())
+cons {CONNECTION}
+
 const SERVER_PORT= 3993
 
-app.use(express.json())
 
 
 app.listen(SERVER_PORT, () => console.log(`Hey Hey, Your Server, Works Today ${SERVER_PORT}`));
